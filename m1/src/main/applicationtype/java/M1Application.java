b907beccd3188eec139462389f9acc441cf7e217
AlieYangpai <yangpai_beibei@163.com>
2019-07-28 23:59:54 +0800

    add mudule type to m1

---
+++ m1/src/main/applicationtype/java/M1Application.java
@@ -0,0 +1,15 @@
+package org.alie.m1;
+
+import android.app.Application;
+
+/**
+ * Created by Alie on 2019/7/28.
+ * 类描述
+ * 版本
+ */
+public class M1Application extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
